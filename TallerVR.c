6e8b999c50d178ad6ed48fab2c41caf2cd4288e8
breadd1 <bmunoz.m26@gmail.com>
2025-05-30 09:06:03 -0400

    Update TallerVR.c

--- TallerVR.c
+++ TallerVR.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int rut = 0, error = 0, compara = 0, x[8], i = 0;
+    int rut = 0, error = 0, compara = 0, x[9], i = 0, z = 2;
     char dv; // dv es el digito verificador
     printf("\nIngrese RUT a verificar (Ej: 12345678): ");
     scanf("%d", &rut);
@@ -17,20 +17,34 @@ int main() {
     }
     
     if (rut <= 99999999 && rut >= 10000000) { //Verificador de rut con 8 digitos.
-    }else { 
+    }else { //cambiar a else if
         if ((rut >= 0 && rut < 10000000) || (rut > 99999999) ){
             error ++;
         }
     }
-    if ((dv != 'K') && ('0') && ('1') && ('2') && ('3') && ('4') && ('5') && ('6') && ('7') && ('8') && ('9')) { //Verificador del digito fuera de rango.
-        error ++;
+    if (dv != 'K' && dv != '0' && dv != '1' && dv != '2' && dv != '3' && dv != '4' && dv != '5' && dv != '6' && dv != '7' && dv != '8' && dv != '9') {
+        error++;
     }
     if (error >= 1) {
-        printf("RUT o Digito verificador incorrecto!!");
+        printf("RUT o Digito verificador incorrecto!!\n");
     } else {
         if (error == 0) {
-            printf("\nEl rut es correcto!");
+            printf("\nEl rut es correcto!\n");
         }
     }
+    i = 0;
+    while(i != 8){
+        x[i] = x[i] * z;
+        z++;
+        i++;
+        if(z == 8){
+            z = 2;
+        }
+        printf("%d", x[i]);
+    }
+    for(i = 0; i = 7; i++){
+        x[9] += x[i];
+    }
+    printf(" %d\n", x[9]);
     return 0;    
 }
